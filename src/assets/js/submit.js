9818bbe4c415ab818f9b532dcc8f249963a3da23
psh4804 <psh480477@gmail.com>
2024-01-17 09:15:27 +0900

    5차 수정

--- src/assets/js/submit.js
+++ src/assets/js/submit.js
@@ -1,10 +1,20 @@
 export function submit() {
     const form = document.querySelector('form');
-    if (!form) return; // 폼이 없으면 함수 종료
+    if (!form) return;
 
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // 폼의 기본 제출 동작 방지
-        alert("감사합니다!"); // 알림 표시
-        window.location.href = 'https://bfsvisitor.netlify.app'; // 리디렉션할 URL 지정
+        event.preventDefault(); // 기본 제출 동작 방지
+
+        var formData = new FormData(event.target);
+        var actionUrl = event.target.action;
+
+        fetch(actionUrl, {
+            method: 'POST',
+            body: formData,
+            mode: 'no-cors'
+        }).then(() => {
+            alert("Complete!");
+            window.location.href = 'https://bfsvisitor.netlify.app';
+        }).catch(error => console.error('Error:', error));
     });
 }
